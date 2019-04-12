f64c9c543b8f16ee2e50f4eeda8192fc863413b4
Robert Preda <predarobert7@gmail.com>
2019-04-13 00:16:12 +0300

    Now graphic test and train r2 scores

--- grad_boost.py
+++ grad_boost.py
@@ -4,6 +4,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error,mean_squared_log_error, r2_score 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 df = pd.read_csv('./data/train.csv')
 X_Data = df[['MSSubClass','LotArea','OverallQual','OverallCond','YearBuilt']]
@@ -26,9 +27,15 @@ print("Predictions: ", len(predictions))
 print("Shape of predictions: ",predictions.shape)
 print("Y shape: ", y_test.shape)
 residue = y_test - predictions
+train, test = [],[]
 for i in range(1, num_of_learners):
     ensemble[i].fit(X_train[:len(residue)], residue)
     predictions = ensemble[i].predict(X_test)
     residue = y_test - predictions
     valid = r2_score(y_test, ensemble[i].predict(X_test))
+    train.append(r2_score(y_train, ensemble[i].predict(X_train)))
+    test.append(r2_score(y_test, ensemble[i].predict(X_test)))
     print("Validation: ", valid)
+plt.plot(train, label = "train",color = 'red')
+plt.plot(test,'bo',label = "test")
+plt.show()
