d0f1359cbff16977b54e2bcd48ad1538a10d3938
Robert Preda <predarobert7@gmail.com>
2019-04-16 10:35:06 +0300

    Actually getting some good results, I think

--- grad_boost.py
+++ grad_boost.py
@@ -20,32 +20,32 @@ depth = int(input("Enter max depth of learners: "))
 # training (I think idk I'm stupid pls help)
 learner = DecisionTreeRegressor(max_depth = depth)
 learner.fit(X_train,y_train)
-predictions = learner.predict(X_test)
+predictions = 0
+predictions += learner.predict(X_test)
 ensemble.append(learner)
-#print("Predictions: ", len(predictions))
-#print("Shape of predictions: ",predictions.shape)
-#print("Y shape: ", y_test.shape)
 residue = y_test - predictions
 train, test = [],[]
 residue_list = [residue]
+new_train = residue + predictions
 for i in range(1, num_of_learners):
     learner = DecisionTreeRegressor(max_depth = depth)
+    
     learner.fit(X_train[:len(residue)],residue)
-    #ensemble[i].fit(X_train[:len(residue)], residue)
-    predictions = learner.predict(X_test)
+    predictions += learner.predict(X_test)
     residue = y_test - predictions
+    new_train += residue
     residue_list.append(residue)
     ensemble.append(learner)
+    train.append(r2_score(y_train, ensemble[i].predict(X_train)))
+    test.append(r2_score(y_test, ensemble[i].predict(X_test)))
 
-y_pred = np.sum(e.predict(X_test) for e in ensemble)
-print("Values of residue: ", residue_list)
-#valid = r2_score(y_test, ensemble[i].predict(X_test))
-train.append(r2_score(y_train, ensemble[i].predict(X_train)))
-test.append(r2_score(y_test, ensemble[i].predict(X_test)))
-score = r2_score(y_test,y_pred)
+score = r2_score(y_test,predictions)
 print("R2 score: ", score)
+'''
 plt.plot(train, label = "train",color = 'red')
-plt.plot(test,'ro',label = "test")
-plt.plot(residue,'bs',label = "residue")
+plt.plot(test,'g',label = "test")
+plt.plot(residue,'bo',label = "residue")
+plt.grid()
 plt.legend()
 plt.show()
+'''
